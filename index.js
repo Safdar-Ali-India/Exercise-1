fb60eb9d73ceb8cd22b6013f7fa46e3e7190ae90
Safdar-Ali-India <72161528+Safdar-Ali-India@users.noreply.github.com>
2023-10-18 08:43:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+//ques 1
+let a="Safdar"
+let b = 10
+  console.log(a+b)
+//ques 2
+console.log(typeof (a+b))
+//ques 3
+const a1={
+  name:"Safdar",
+  section:1,
+  isPrincipal:false
+}
+//a1=45
+//ques 4
+a1['friend']="Shubham"
+a1['name']="Ali"
+console.log(a1)
+
+//ques 5
+const dict={
+  hibernation : "a state of winter dormancy in some animals",
+  screaming: "extreme nervousness",
+  safdar: "a developer",
+}
+console.log(dict['hibernation'])
